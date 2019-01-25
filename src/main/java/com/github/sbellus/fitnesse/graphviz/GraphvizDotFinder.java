b42ce265f9be4da6630d8c898ac24031f5542b10
Štefan Bellus <stefan.bellus@innovatrics.com>
2019-01-25 15:09:05 +0100

    Fixed issue #2. The plugin now depends on Fitnesse version 20190119.

--- src/main/java/com/github/sbellus/fitnesse/graphviz/GraphvizDotFinder.java
+++ src/main/java/com/github/sbellus/fitnesse/graphviz/GraphvizDotFinder.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.commons.lang.SystemUtils;
+import org.apache.commons.lang3.SystemUtils;
 
 public class GraphvizDotFinder {
     
